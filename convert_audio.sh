2f26be8de2338ec743e66547eeb8b615b4f1e27e
chenchuhui hu <chenchuhuihu@crc-dot1x-nat-10-239-31-79.bu.edu>
2023-11-10 15:24:32 -0500

    Add script for converting audio, update gitignore

---
+++ convert_audio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loop through all .mp3 files in the current directory
+for file in Icelandic_isl_ICE_NT_Non-Drama/*.mp3; do
+  # Use basename to get the file name without the extension
+  base_name=$(basename "$file" .mp3)
+  
+  # Construct the output file path with the new extension
+  output="Icelandic_isl_ICE_NT_Non-Drama_Converted/${base_name}.wav"
+  
+  # Run ffmpeg command
+  ffmpeg -i "$file" -ar 16000 -ac 1 -ss 00:00:04 "$output"
+done
